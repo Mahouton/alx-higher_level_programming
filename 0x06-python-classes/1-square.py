1c806907ca20172ecbe19e88af204f8de3b7d0b4
Paul KPADONOU <pmkpaul87@gmail.com>
2022-06-22 01:26:53 +0100

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines Square class
+"""
+
+
+class Square:
+    """Defines a square by:
+    private instance attribute: size
+    """
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Square class definition
+"""
+
+
+class Square:
+    """
+    Square class with private instance attribute size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """size: size of current square
+        setter validating size is not int and >= 0
+
+        Raise:
+            TypeError and ValueError
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """size: size of square
+        setter validating size is int and >= 0
+
+        Raise:
+            TypeError and ValueError
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Returns area of the current square
+        """
+        return (self.size ** 2)
